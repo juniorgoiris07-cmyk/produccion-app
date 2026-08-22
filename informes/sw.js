120b55e9973c352ab567ff449664123db92d3e9b
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-08-22 10:09:15 -0300

    Agrega resumen por turno (toneladas y lotes de hoy) en Informes

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v6';
+const CACHE_NAME = 'informes-produccion-v7';
 const ASSETS = [
   './',
   './index.html',
