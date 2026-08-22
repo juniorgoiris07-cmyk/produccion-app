6904fa1170357dd9787e097820c9a354a91b32f3
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-08-22 10:46:40 -0300

    Adapta Informes a los nuevos campos de la planilla (BB, temp ambiente/grano, humedad de grano, encargado)

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v10';
+const CACHE_NAME = 'informes-produccion-v11';
 const ASSETS = [
   './',
   './index.html',
