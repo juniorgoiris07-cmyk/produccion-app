55a63c85ec99049955ad5847f7df25dae73d01b0
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-08-22 15:55:48 -0300

    Soporta fecha DD-MM-AA (2 cifras) que ahora usa la planilla

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v14';
+const CACHE_NAME = 'informes-produccion-v15';
 const ASSETS = [
   './',
   './index.html',
