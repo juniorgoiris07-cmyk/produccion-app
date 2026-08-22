9908db00691f9976c6df456daed0d2a3ef6a9d5c
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-08-22 07:19:24 -0300

    Rediseño visual de Informes: header, animaciones y microinteracciones

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v3';
+const CACHE_NAME = 'informes-produccion-v4';
 const ASSETS = [
   './',
   './index.html',
