3cb5a9ff3c3ae139c296a1fd588da5a0ca8e7cef
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-08-24 07:22:26 -0300

    Selector de vistas (Turno/Lote/Bloques de 25 Tons/BigBag); corrige que un lote no tiene tope de 25 tons; saca emojis

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v15';
+const CACHE_NAME = 'informes-produccion-v16';
 const ASSETS = [
   './',
   './index.html',
