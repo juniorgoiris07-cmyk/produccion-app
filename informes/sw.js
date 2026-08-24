013ede88956c935b69ffc81019626cb5620e6f29
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-08-24 08:14:19 -0300

    Colores mas practicos: mejor contraste en textos dorados, botones mas grandes para tocar, y ajustes de legibilidad

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v20';
+const CACHE_NAME = 'informes-produccion-v21';
 const ASSETS = [
   './',
   './index.html',
