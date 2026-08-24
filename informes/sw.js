45080862c0ad5443cc196d01cedb57e70f77b83e
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-08-24 07:33:30 -0300

    Simplifica a 3 vistas (Turno/Lote/BigBag), saca la vista de Bloques que era redundante

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v16';
+const CACHE_NAME = 'informes-produccion-v17';
 const ASSETS = [
   './',
   './index.html',
