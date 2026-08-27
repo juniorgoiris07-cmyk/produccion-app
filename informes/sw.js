50c915bfa0f5b17c497ab068768a9b4126b488d4
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-08-27 08:39:26 -0300

    Add files via upload

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v26';
+const CACHE_NAME = 'informes-produccion-v27';
 const ASSETS = [
   './',
   './index.html',
