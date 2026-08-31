83fc8eb5b0847719033d4d0b6e737b9047f61fc3
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-08-31 11:31:29 -0300

    Fix: turno Noche cargado con demora no corta la tanda (v29)

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v28';
+const CACHE_NAME = 'informes-produccion-v29';
 const ASSETS = [
   './',
   './index.html',
