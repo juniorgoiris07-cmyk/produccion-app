127a3422e87551456b17b90a8e099537b7ae5bde
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-09-11 07:20:25 -0300

    Producción: selector de meses con checkboxes + atajos (mas claro)

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v33';
+const CACHE_NAME = 'informes-produccion-v34';
 const ASSETS = [
   './',
   './index.html',
