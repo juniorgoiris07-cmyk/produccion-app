02881e24eea31ea9cb6b9eab211d4bc2018363a0
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-09-15 10:20:24 -0300

    Vista Por Calidad: reemplaza 25 Tons con 4 cuadros y asignacion de clientes

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v36';
+const CACHE_NAME = 'informes-produccion-v37';
 const ASSETS = [
   './',
   './index.html',
