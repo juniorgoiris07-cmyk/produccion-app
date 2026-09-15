818a141ba105bf309c209969d6c5d698e021e95f
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-09-15 14:46:32 -0300

    Rediseñar selector de vista en Producción: menú de cuadros en vez de pestañas

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v40';
+const CACHE_NAME = 'informes-produccion-v41';
 const ASSETS = [
   './',
   './index.html',
