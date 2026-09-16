5bfd39171e81ccdee24561f3797f8542609aeeb9
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-09-16 11:18:46 -0300

    Ventas como paginas propias (no popup) + bump sw v53

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v52';
+const CACHE_NAME = 'informes-produccion-v53';
 const ASSETS = [
   './',
   './index.html',
