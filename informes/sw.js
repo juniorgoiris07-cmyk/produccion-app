4cac28dcd17f6c43f3ab301e2bc8053b8131088d
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-09-18 07:14:30 -0300

    Cierre de Lotes: permiso aparte por persona

--- informes/sw.js
+++ informes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'informes-produccion-v59';
+const CACHE_NAME = 'informes-produccion-v60';
 const ASSETS = [
   './',
   './index.html',
